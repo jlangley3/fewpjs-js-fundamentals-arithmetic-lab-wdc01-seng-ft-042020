9402111b5800a268e05129ac0e1f3b101522bc93
jlangley3 <jlangleyiii@aol.com>
2020-05-30 19:58:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+
+let random = Math.random();
+
+let mod = 64 % 5
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
+
+let num1 = 2
+let num2 = 31
+let multiply = ((num1) * (num2));
